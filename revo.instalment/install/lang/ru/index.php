b990e2969f541b196e0d4442c089859d64691d18
gvammer <gvamm3r@gmail.com>
2019-04-02 01:23:34 +0300

    v0.2

--- revo.instalment/install/lang/ru/index.php
+++ revo.instalment/install/lang/ru/index.php
@@ -2,7 +2,7 @@
 $MESS['REVO_MODULE_NAME'] = 'Revo.Instalment';
 $MESS['REVO_MODULE_DESCRIPTION'] = '';
 $MESS['REVO_MODULE_PARTNER_NAME'] = 'REVO';
-$MESS['REVO_MODULE_PAYMENT_NAME'] = 'Revo Instalment';
-$MESS['REVO_MODULE_PAYMENT_DESC'] = 'Оформление рассрочки от REVO';
+$MESS['REVO_MODULE_PAYMENT_NAME'] = 'REVO Рассрочка';
+$MESS['REVO_MODULE_PAYMENT_DESC'] = 'Оформление рассрочки на 6 месяцев';
 $MESS['REVO_MODULE_PAYMENT_FAIL'] = 'Не удалось добавить платежную систему.';
 $MESS['REVO_MODULE_SALE_FAIL'] = 'Даный плагин работает только с модулем "Интернет-магазин"';
