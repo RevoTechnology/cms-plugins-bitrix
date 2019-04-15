d90dd2e5ce73242e0acd97c6937b5e6ce1640cce
gvammer <gvamm3r@gmail.com>
2019-04-15 17:58:45 +0300

    component and snippet

--- revo.instalment/install/components/buy.link/templates/.default/template.php
+++ revo.instalment/install/components/buy.link/templates/.default/template.php
@@ -2,4 +2,4 @@
 if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
 ?>
 <a href="#" class="js-rvo-buy-link"
-   data-item="<?=$arParams['ITEM_ID']?>">Купить за <?=$arParams['MONTHLY_PRICE']?> руб. в месяц</a>
+   data-item="<?=$arParams['ITEM_ID']?>">Купить за <?=round($arParams['MONTHLY_PRICE'] / 3)?> руб. в месяц</a>
