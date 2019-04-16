318a8f71867a54b4b27796618137d57e1875d0fe
emaslov <e.maslov@mangotele.com>
2019-04-16 13:44:55 +0300

    snippets install

--- revo.instalment/install/index.php
+++ revo.instalment/install/index.php
@@ -203,6 +203,7 @@ class revo_instalment extends CModule
         CopyDirFiles($_SERVER['DOCUMENT_ROOT'] . '/local/modules/' . $this->MODULE_ID . '/install/sale_payment/', $_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/include/sale_payment', true, true);
         CopyDirFiles($_SERVER['DOCUMENT_ROOT'] . '/local/modules/' . $this->MODULE_ID . '/install/ajax/', $_SERVER['DOCUMENT_ROOT'] . '/ajax/'.$this->MODULE_ID.'/', true);
         CopyDirFiles($_SERVER['DOCUMENT_ROOT'] . '/local/modules/' . $this->MODULE_ID . '/install/components/', $_SERVER['DOCUMENT_ROOT'] . '/local/components/revo/', true);
+        CopyDirFiles($_SERVER['DOCUMENT_ROOT'] . '/local/modules/' . $this->MODULE_ID . '/install/snippets/', $_SERVER['DOCUMENT_ROOT'] . '/bitrix/templates/.default/snippets/', true);
         mkdir($_SERVER['DOCUMENT_ROOT'] . '/upload/check/');
         return true;
     }
@@ -214,6 +215,7 @@ class revo_instalment extends CModule
         DeleteDirFiles($_SERVER['DOCUMENT_ROOT'] . '/local/modules/' . $this->MODULE_ID . '/install/sale_payment/', $_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/include/sale_payment');
         DeleteDirFiles($_SERVER['DOCUMENT_ROOT'] . '/local/modules/' . $this->MODULE_ID . '/install/ajax/', $_SERVER['DOCUMENT_ROOT'] . '/ajax/'.$this->MODULE_ID.'/');
         DeleteDirFiles($_SERVER['DOCUMENT_ROOT'] . '/local/modules/' . $this->MODULE_ID . '/install/components/', $_SERVER['DOCUMENT_ROOT'] . '/local/components/revo/');
+        DeleteDirFiles($_SERVER['DOCUMENT_ROOT'] . '/local/modules/' . $this->MODULE_ID . '/install/snippets/', $_SERVER['DOCUMENT_ROOT'] . '/bitrix/templates/.default/snippets/');
         rmdir($_SERVER['DOCUMENT_ROOT'] . '/upload/check/');
         return true;
     }
