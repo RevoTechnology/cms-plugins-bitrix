96f8ccbe0c3a5bb54e739aee1a84fb236c824717
gvammer <gvamm3r@gmail.com>
2019-05-08 16:47:20 +0300

    - add mobile size for iframe

--- revo.instalment/lang/ru/js.php
+++ revo.instalment/lang/ru/js.php
@@ -1,2 +1,2 @@
 <?php
-$MESS['REVO_BUY_DETAIL'] = 'Оплата&nbsp;частями&nbsp;от&nbsp;<b>#PRICE#</b>&nbsp;руб./мес.';
+$MESS['REVO_BUY_DETAIL'] = 'Оплата&nbsp;частями от&nbsp;<b>#PRICE#</b>&nbsp;руб./мес.';
