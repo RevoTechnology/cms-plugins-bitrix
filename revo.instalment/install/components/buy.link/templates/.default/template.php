2c7e4aeef61c72f69aebabab32a0f5ae92851c10
emaslov <e.maslov@mangotele.com>
2019-06-10 17:30:59 +0300

    Hide component for users in debug mode

--- revo.instalment/install/components/buy.link/templates/.default/template.php
+++ revo.instalment/install/components/buy.link/templates/.default/template.php
@@ -1,10 +1,15 @@
 <?php
 if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
+use Bitrix\Main\Config\Option;
 
-define('INSTALMENT_PERIOD', 12);
-?>
-<a href="#" class="js-rvo-buy-link"
-   data-item="<?=$arParams['ITEM_ID']?>"
-   data-buybtn="<?=$arParams['BUY_BTN_SELECTOR']?>">
-    Оплата частями от <?=round($arParams['PRICE'] / INSTALMENT_PERIOD)?> руб. в месяц.
-</a>
+$showBlock = Option::get('revo.instalment', 'debug_mode', 'Y') != 'Y' || $USER->IsAdmin();
+if ($showBlock) {
+    define('INSTALMENT_PERIOD', 12);
+    ?>
+    <a href="#" class="js-rvo-buy-link"
+       data-item="<?= $arParams['ITEM_ID'] ?>"
+       data-buybtn="<?= $arParams['BUY_BTN_SELECTOR'] ?>">
+        Оплата частями от <?= round($arParams['PRICE'] / INSTALMENT_PERIOD) ?> руб. в месяц.
+    </a>
+    <?php
+}
