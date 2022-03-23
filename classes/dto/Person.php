7264df2d9cf0d104d18f89ca54a09520315903f6
Kulba <danilkulba@yandex.ru>
2022-03-23 18:23:42 +0300

    BTB-363: reworked logic how we fill name of customer

--- classes/dto/Person.php
+++ classes/dto/Person.php
@@ -15,12 +15,38 @@ class Person
         $this->patronymic = $patronymic;
     }
 
+    /**
+     * Сначала берем ФИО из личного кабинета пользователя.
+     * Если там нет - берем из формы.
+     * Отчество почти всегда будет браться из формы.
+     * $arName[0] - фамилия
+     * $arName[1] - имя
+     * $arName[2] - отчество
+     */
     public static function getFromGlobalParams($arParams)
     {
+        // ФИО которое вводят на этапе оформления заказа
+        $arName = explode(' ', $arParams['PROPERTY']['FIO']);
+
         $obj = new self('', '', '');
-        $obj->first_name = $arParams['PROPERTY']['FIO'] ? array_pop(explode(' ', $arParams['PROPERTY']['FIO'], 1)) : $arParams['USER']['NAME'];
-        $obj->surname = $arParams['PROPERTY']['FIO'] && strpos($arParams['PROPERTY']['FIO'], ' ') ?
-            array_shift(explode(' ', $arParams['PROPERTY']['FIO'], 1)) : $arParams['USER']['LAST_NAME'];
+        // фамилия
+        if (!empty($arParams['USER']['LAST_NAME']))
+            $obj->surname = $arParams['USER']['LAST_NAME'];
+        elseif(!empty($arName[0]))
+            $obj->surname = $arName[0];
+
+        // имя
+        if (!empty($arParams['USER']['NAME']))
+            $obj->first_name = $arParams['USER']['NAME'];
+        elseif(!empty($arName[1]))
+            $obj->first_name = $arName[1];
+
+        // отчество
+        if (!empty($arParams['USER']['SECOND_NAME']))
+            $obj->patronymic = $arParams['USER']['SECOND_NAME'];
+        elseif(!empty($arName[2]))
+            $obj->patronymic = $arName[2];
+
         return $obj;
     }
 }
